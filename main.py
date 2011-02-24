CaptainPatate/ascfpmfsrt

commit 17995d24797b91209c8d072347d45bf1ec2fa938

Throw impostor out!
If a logged user doesn't appear in our authorized users db, we forbid the access.

main.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import os
 
 from google.appengine.api import users
@@ -6,14 +7,30 @@ from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from models import Cfp
+from models import Cfp, AuthorizedUser
 
 # name, fullname, website, begin_conf_date, end_conf_date, submission_deadline,
 # notification_date, country, city, acceptance_rate, submitters, category, keywords
 # last_update
 
+def authenticationRequired(user, handler):
+    auth = AuthorizedUser.all()
+    auth.filter('uid =', user.user_id())
+
+    if not auth.get():
+        logging.info('The unauthorized user "%s (%s)" tried to connect.',
+                     user.nickname(), user.email())
+        handler.redirect('/out')
+
+class OutHandler(webapp.RequestHandler):
+    def get(self):
+        html = os.path.join(os.path.dirname(__file__), 'templates/not_authorized.html')
+        self.response.out.write(template.render(html, {'logout_url': users.create_logout_url("/")}))
+
+
 class CfpView(webapp.RequestHandler):
     def get(self):
+        authenticationRequired(users.get_current_user(), self)
 
         cfps = Cfp.all()
         cfps.filter('submission_deadline >=', datetime.date.today())
@@ -38,13 +55,14 @@ class AddCfpHandler(webapp.RequestHandler):
         return datetime.date(int(date[0]),int(date[1]),int(date[2]))
 
     def get(self, nothing):
+        authenticationRequired(users.get_current_user(), self)
+
         html = os.path.join(os.path.dirname(__file__), 'templates/add_cfp.html')
         self.response.out.write(template.render(html, {'logout_url': users.create_logout_url("/")}))
 
     def post(self, cfpid=None):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+        authenticationRequired(users.get_current_user(), self)
+
         if not cfpid:
             cfp = Cfp()
         else:
@@ -69,9 +87,7 @@ class AddCfpHandler(webapp.RequestHandler):
 
 class UpdateHandler(webapp.RequestHandler):
     def get(self, what, cfpid):
-        user = users.get_current_user()
-        if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+        authenticationRequired(users.get_current_user(), self)
 
         if what == 'submit':
             cfp = db.get(cfpid)
@@ -95,25 +111,21 @@ class FeedHandler(webapp.RequestHandler):
             cfps.filter('submission_deadline >=', datetime.date.today())
             cfps.order('submission_deadline').fetch(limit=50)
         elif what == 'submitters':
-            user = users.get_current_user()
-            if user:
-                cfps = Cfp.all()
-                cfps.filter('submission_deadline >=', datetime.date.today())
-                cfps.order('submission_deadline').fetch(limit=50)
-                # we can't make a query with two inequality conditions
-                # so we filter in python in the pending of model redesign
-                cfps = filter(lambda cfp: cfp.submitters != [], cfps)
-            else:
-                self.redirect(users.create_login_url(self.request.uri))
+            authenticationRequired(users.get_current_user(), self)
+
+            cfps = Cfp.all()
+            cfps.filter('submission_deadline >=', datetime.date.today())
+            cfps.order('submission_deadline').fetch(limit=50)
+            # we can't make a query with two inequality conditions
+            # so we filter in python in the pending of model redesign
+            cfps = filter(lambda cfp: cfp.submitters != [], cfps)
         elif what == 'submitter':
-            user = users.get_current_user()
-            if user:
-                cfps = Cfp.all()
-                cfps.filter('submission_deadline >=', datetime.date.today())
-                cfps.filter('submitters IN', [users.User(userid.replace('%40','@'))])
-                cfps.order('submission_deadline').fetch(limit=50)
-            else:
-                self.redirect(users.create_login_url(self.request.uri))
+            authenticationRequired(users.get_current_user(), self)
+
+            cfps = Cfp.all()
+            cfps.filter('submission_deadline >=', datetime.date.today())
+            cfps.filter('submitters IN', [users.User(userid.replace('%40','@'))])
+            cfps.order('submission_deadline').fetch(limit=50)
         else:
             self.response.set_status(404, 'Not Found')
             return      
@@ -125,6 +137,7 @@ class FeedHandler(webapp.RequestHandler):
 
 
 application = webapp.WSGIApplication([(r'/', CfpView),
+                                      (r'/out', OutHandler),
                                       (r'/addcfp/(.*)', AddCfpHandler),
                                       (r'/(submit)/(.*)', UpdateHandler),
                                       (r'/(update)/(.*)', UpdateHandler),

models.py
@@ -37,3 +37,7 @@ class Cfp(db.Model):
 
     def isSmartcard(self):
         return 'Smartcard' in self.keywords
+
+
+class AuthorizedUser(db.Model):
+    uid = db.StringProperty()

static/style.css
@@ -23,6 +23,13 @@ p {
     margin-top: 0px;
 }
 
+div.warning {
+    border: 1px solid #f00;
+    background-color: #fee;
+    margin-bottom: 1em;
+    padding: 1em;
+}
+
 ul.cfp {
     list-style: none;
     border: solid;

templates/not_authorized.html
@@ -1,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+           "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <meta name="robots" content="noarchive"/>
+    <link rel="stylesheet" href="/static/style.css" type="text/css" media="screen"/>
+    <title>A Simple CFP manager For Small Researcher Team</title>
+  </head>
+
+  <body>
+    <div class="warning">
+      <p>I am sorry but I can't let you continue wander here
+	unpunished. Because you don't have a proper clearance, I am in
+	the obligation to stop you.</p>
+      <p><a href="{{ logout_url|escape }}">Try to login with another
+	  identity, maybe you will be lucky...</a></p>
+    </div>
+    <p>This software has been designed to be used by small researcher
+    team. But it's a free software, you can run your own instance.
+    For more details
+    see <a href="https://github.com/CaptainPatate/ascfpmfsrt">here</a></p>
+  </body>
+</html>

commit f42ca7aa008792f1f781029702ec62eed6a136ba

Correct bug introduced by 17995d2.

main.py
@@ -90,6 +90,7 @@ class UpdateHandler(webapp.RequestHandler):
         authenticationRequired(users.get_current_user(), self)
 
         if what == 'submit':
+            user = users.get_current_user()
             cfp = db.get(cfpid)
             if user not in cfp.submitters:
                 cfp.submitters.append(user)